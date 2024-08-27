7b6c0539db7990d2b9d243c4cf37fb7c69fce772
mosescode1 <emosescode@gmail.com>
2024-08-27 15:30:45 +0100

    get the best language to return to user

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,1 +1,32 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Application Home route"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+app = Flask(__name__)
+
+
+class Config:
+    """Configuration class for app and babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Gets the best match language"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/', methods=["GET"])
+def home():
+    """home route serving the default home page"""
+    return render_template("0-index.html",)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
