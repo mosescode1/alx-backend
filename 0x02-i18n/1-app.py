78f14daa8bd4827316dce10ada7ef945c53868fb
mosescode1 <emosescode@gmail.com>
2024-08-27 15:22:48 +0100

    configured babel to use default locale and timezone

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,25 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Application Home route"""
+from flask import Flask, render_template
+from flask_babel import Babel
+app = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', methods=["GET"])
+def home():
+    """home route serving the default home page"""
+    return render_template("0-index.html",)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
